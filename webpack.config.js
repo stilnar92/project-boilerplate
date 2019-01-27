ed5ee7df885809041e643b78f8fddb07570c3054
stilnar92 <stilnar92.gmail.com>
2019-01-27 13:11:14 +0300

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
