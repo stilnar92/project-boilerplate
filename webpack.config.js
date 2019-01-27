6d0f284c71a352095c059de2d11411a18e0525b4
stilnar92 <stilnar92.gmail.com>
2019-01-27 13:15:20 +0300

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,17 @@ module.exports = {
     },
     devServer: {
         contentBase: './dist'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
 };
